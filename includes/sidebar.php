50c3bf26bc65bab74d796152df853dfdfd50ddd0
zzzhenng <cjlkindle01@gmail.com>
2018-10-03 15:20:09 +0800

    login page and turn to admin

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -6,16 +6,35 @@
       <div class="card-header"><h4>Blog Search</h4></div>
       <div class="card-body">
         <form action="search.php"  method="post">
-            <div class="input-group">
-              <input  name="search" type="text" class="form-control">
-
-              <button name="submit" type="submit" class="btn btn-success">Search
-              </button>
-
+            <div class="form-group">
+              <input  name="search" type="text" class="form-control" placeholder="Search Input">
             </div>
+              <div class="form-group">
+                <button name="submit" type="submit" class="btn btn-success">Search
+                </button>
+              </div>
           </form>
       </div>
     </div><!-- end search-->
+
+    <!-- login -->
+    <div class="card">
+      <div class="card-header"><h4>Blog Login</h4></div>
+      <div class="card-body">
+        <form action="./includes/login.php"  method="post">
+            <div class="form-group">
+              <input  name="username" type="text" class="form-control" placeholder="Enter Username">
+            </div>
+            <div class="form-group">
+              <input  name="password" type="password" class="form-control" placeholder="Enter password">
+            </div>
+            <div class="form-group">
+              <button name="login" type="submit" class="btn btn-success">Login
+              </button>
+            </div>
+          </form>
+      </div>
+    </div><!-- end login-->
 
 
     <!-- blog categories-->
